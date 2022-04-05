882243dd3ece80c9ec65141c7a683e3c14ab6f66
Olia Dovbush <oliaivanchushun@gmail.com>
2022-04-05 13:58:18 +0300

    change adaptive approach

--- src/components/Spacer.jsx
+++ src/components/Spacer.jsx
@@ -4,9 +4,9 @@ import useWindowSize from '../Hooks/UseWindowSize';
 export default function Spacer(props) {
   let { space, style = {}, children } = props;
   const size = useWindowSize();
-  if (size.width <= 375) {
-    space = 0.3 * space;
-  } else if (size.width <= 768) {
+  if (size.width <= 768) {
+    space = 0.4 * space;
+  } else if (size.width <= 1200) {
     space = 0.6 * space;
   } else {
     space = space;
