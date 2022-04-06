644d1e04efe84807c91a2b61da0c935c3da39b6a
Olia Dovbush <oliaivanchushun@gmail.com>
2022-04-06 14:41:37 +0300

    add style to categories

--- src/components/Spacer.jsx
+++ src/components/Spacer.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { getDevice } from '../utils';
 
-export default function Spacer({space, style = {}, children}) {
+export default function Spacer({ space, style = {}, children }) {
   let _space = parseInt(space);
-  const isObj = isNaN( _space );
+  const isObj = isNaN(_space);
 
   const device = getDevice();
 
-  console.log(device);
-
-  // debugger;
-
-  if ( isObj ) {
-    _space = space[ device ] || 0
+  if (isObj) {
+    _space = space[device] || 0;
   }
-
-
 
   return (
     <div
